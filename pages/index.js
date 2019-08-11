d5c10a6a981dd9de84af0909bd714f3201cb1fe3
shinshin86 <beagles1986@gmail.com>
2019-08-11 13:13:36 +0900

    Replace to styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,52 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const Title = styled.div`
+  margin: 0;
+  width: 100%;
+  padding-top: 80px;
+  line-height: 1.15;
+  font-size: 48px;
+  text-align: center;
+`;
+
+const Row = styled.div`
+  max-width: 880px;
+  margin: 80px auto 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const Button = styled.div`
+  text-align: center;
+`;
+
+const FormSubmit = styled.input`
+  background-color: #4385cc;
+  color: white;
+  padding: 16px 32px;
+  border-radius: 8px;
+  text-decoration: none;
+  margin: 4px 2px;
+  cursor: pointer;
+`;
 
 const Home = () => (
   <div>
-    <div className="title">Discogs API Sample</div>
-    <div className="row">
+    <Title>Discogs API Sample</Title>
+    <Row>
       <form action="/releases">
         <label>
           Release ID:
           <input type="text" name="id" />
         </label>
-        <div className="button">
-          <input type="submit" value="Search" />
-        </div>
+        <Button>
+          <FormSubmit type="submit" value="Search" />
+        </Button>
       </form>
-    </div>
-
-    <style jsx>{`
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .button {
-        text-align: center;
-      }
-      input[type='submit'] {
-        background-color: #4385cc;
-        color: white;
-        padding: 16px 32px;
-        border-radius: 8px;
-        text-decoration: none;
-        margin: 4px 2px;
-        cursor: pointer;
-      }
-    `}</style>
+    </Row>
   </div>
 );
 
--- pages/releases.js
+++ pages/releases.js
@@ -1,6 +1,76 @@
 import React from 'react';
 import Link from 'next/link';
 import 'isomorphic-unfetch';
+import styled from 'styled-components';
+
+const Hero = styled.div`
+  width: 100%;
+  color: #333;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  width: 100%;
+  padding-top: 80px;
+  line-height: 1.15;
+  font-size: 48px;
+  text-align: center;
+`;
+
+const Description = styled.p`
+  text-align: center;
+`;
+
+const Row = styled.div`
+  margin: 80px auto 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const Notes = styled.div`
+  padding: 16px;
+  margin: 16px auto;
+  max-width: 520px;
+  text-align: left;
+  text-decoration: none;
+  color: #434343;
+  border: 1px solid #9b9b9b;
+`;
+
+const Card = styled.div`
+  padding: 16px;
+  width: 320px;
+  text-align: left;
+  text-decoration: none;
+  color: #434343;
+  border: 1px solid #9b9b9b;
+  :hover {
+    border-color: #067df7;
+  }
+`;
+
+const Subcard = styled.div`
+  margin: 16px;
+  padding: 16px;
+  text-align: left;
+  text-decoration: none;
+  color: #434343;
+  border: 1px solid #9b9b9b;
+`;
+
+const ArtistName = styled.h3`
+  margin: 0;
+  color: #067df7;
+  font-size: 18px;
+`;
+
+const ArtistDescription = styled.p`
+  margin: 0;
+  padding: 12px 0 0;
+  font-size: 13px;
+  color: #333;
+`;
 
 export default class Releases extends React.Component {
   static async getInitialProps({ query }) {
@@ -29,139 +99,77 @@ export default class Releases extends React.Component {
 
     return (
       <div>
-        <div className="hero">
-          <h1 className="title">{title}</h1>
-          <div className="notes">
-            <p className="description">{notes}</p>
-          </div>
-          <p className="description">
+        <Hero>
+          <Title>{title}</Title>
+          <Notes>
+            <Description>{notes}</Description>
+          </Notes>
+          <Description>
             <a href={uri} target="_blnak">
               check this music at Discogs
             </a>
-          </p>
-          <p className="description">
+          </Description>
+          <Description>
             Get release data using{' '}
             <a href="http://www.discogs.com/developers/" target="_blnak">
               Discogs.com API v2.0.
             </a>
-          </p>
+          </Description>
 
-          <div className="row">
-            <div className="card">
-              <h3>Artist</h3>
+          <Row>
+            <Card>
+              <ArtistName>Artist</ArtistName>
               {artists.map((artist, index) => (
-                <div className="subcard">
+                <Subcard>
                   <a href={artist.resource_url} key={index}>
-                    <h3>{artist.name}</h3>
+                    <ArtistName>{artist.name}</ArtistName>
                   </a>
-                  <p className="description">{artist.detail.profile}</p>
+                  <ArtistDescription>{artist.detail.profile}</ArtistDescription>
                   {artist.detail.urls.map(url => (
-                    <p>
+                    <ArtistDescription>
                       <a href={url}>{url}</a>
-                    </p>
+                    </ArtistDescription>
                   ))}
-                </div>
+                </Subcard>
               ))}
-            </div>
-            <div className="card">
-              <h3>Extra Aritists</h3>
+            </Card>
+            <Card>
+              <ArtistName>Extra Aritists</ArtistName>
               {extraartists.map((ext, index) => (
-                <div className="subcard">
+                <Subcard>
                   <a href={ext.resource_url} key={index}>
-                    <h3>{ext.name}</h3>
-                    <p>{ext.role}</p>
+                    <ArtistName>{ext.name}</ArtistName>
+                    <ArtistDescription>{ext.role}</ArtistDescription>
                   </a>
-                </div>
+                </Subcard>
               ))}
-            </div>
-            <div className="card">
-              <h3>Company</h3>
+            </Card>
+            <Card>
+              <ArtistName>Company</ArtistName>
               {companies.map((company, index) => (
-                <div className="subcard">
+                <Subcard>
                   <a href={company.resource_url} key={index}>
-                    <h3>{company.name}</h3>
-                    <p>{company.entity_type_name}</p>
+                    <ArtistName>{company.name}</ArtistName>
+                    <ArtistDescription>
+                      {company.entity_type_name}
+                    </ArtistDescription>
                   </a>
-                </div>
+                </Subcard>
               ))}
-            </div>
-            <div className="card">
-              <h3>Label</h3>
+            </Card>
+            <Card>
+              <ArtistName>Label</ArtistName>
               {labels.map((label, index) => (
-                <div className="subcard">
+                <Subcard>
                   <a href={label.resource_url} key={index}>
-                    <h3>{label.name}</h3>
-                    <p>{label.catno}</p>
+                    <ArtistName>{label.name}</ArtistName>
+                    <ArtistDescription>{label.catno}</ArtistDescription>
                   </a>
-                </div>
+                </Subcard>
               ))}
-            </div>
-          </div>
-        </div>
-
-        <style jsx>{`
-          .hero {
-            width: 100%;
-            color: #333;
-          }
-          .title {
-            margin: 0;
-            width: 100%;
-            padding-top: 80px;
-            line-height: 1.15;
-            font-size: 48px;
-            text-align: center;
-          }
-          .title,
-          .description {
-            text-align: center;
-          }
-          .row {
-            margin: 80px auto 40px;
-            display: flex;
-            flex-direction: row;
-            justify-content: space-around;
-          }
-          .notes {
-            padding: 16px;
-            margin: 16px auto;
-            max-width: 520px;
-            text-align: left;
-            text-decoration: none;
-            color: #434343;
-            border: 1px solid #9b9b9b;
-          }
-          .card {
-            padding: 16px;
-            width: 320px;
-            text-align: left;
-            text-decoration: none;
-            color: #434343;
-            border: 1px solid #9b9b9b;
-          }
-          .subcard {
-            margin: 16px;
-            padding: 16px;
-            text-align: left;
-            text-decoration: none;
-            color: #434343;
-            border: 1px solid #9b9b9b;
-          }
-          .card:hover {
-            border-color: #067df7;
-          }
-          .card h3 {
-            margin: 0;
-            color: #067df7;
-            font-size: 18px;
-          }
-          .card p {
-            margin: 0;
-            padding: 12px 0 0;
-            font-size: 13px;
-            color: #333;
-          }
-        `}</style>
+            </Card>
+          </Row>
+        </Hero>
       </div>
     );
   }
